a025d66e9ce6ddeae9a561cf62b44d52652a5f35
zhikh <zhikh.k@gmail.com>
2025-01-27 00:44:43 +0300

    fix: исправлена утечка памяти при удалении узла

--- src/bstree.c
+++ src/bstree.c
@@ -59,26 +59,26 @@ bstree_node_t *bstree_node_create(const mkey_t key, const mval_t value) {
     return node;
 }
 
-static bstree_node_t *bstree_node_insert(bstree_node_t *self, const mkey_t key, const mval_t value) {
+static bstree_node_t *bstree_node_insert(bstree_node_t *node, const mkey_t key, const mval_t value) {
     assert(key);
 
-    // If root does not exist, new node will be root of bstree.
-    if (self == NULL) {
-        bstree_node_t *node = bstree_node_create(key, value);
-        if (IS_ERR(node))
-            return ERR_CAST(node);
-        return node;
+    // Если корня нет, то новый узел становится корнем.
+    if (node == NULL) {
+        bstree_node_t *new_root = bstree_node_create(key, value);
+        if (IS_ERR(new_root))
+            return ERR_CAST(new_root);
+        return new_root;
     }
 
-    const int cmp = strcmp(key, self->data.key);
+    const int cmp = strcmp(key, node->data.key);
     if (cmp < 0)
-        self->left = bstree_node_insert(self->left, key, value);
+        node->left = bstree_node_insert(node->left, key, value);
     else if (cmp > 0)
-        self->right = bstree_node_insert(self->right, key, value);
+        node->right = bstree_node_insert(node->right, key, value);
     else
-        self->data.value = value;
+        node->data.value = value;
 
-    return self;
+    return node;
 }
 
 void bstree_insert(void *_self, const mkey_t key, const mval_t value) {
@@ -86,19 +86,19 @@ void bstree_insert(void *_self, const mkey_t key, const mval_t value) {
     self->root = bstree_node_insert(self->root, key, value);
 }
 
-static bstree_node_t *bstree_node_lookup(bstree_node_t *self, const char *key) {
+static bstree_node_t *bstree_node_lookup(bstree_node_t *node, const char *key) {
     assert(key);
 
-    if (self == NULL)
+    if (node == NULL)
         return NULL;
 
-    const int cmp = strcmp(key, self->data.key);
+    const int cmp = strcmp(key, node->data.key);
     if (cmp < 0)
-        return bstree_node_lookup(self->left, key);
+        return bstree_node_lookup(node->left, key);
     if (cmp > 0)
-        return bstree_node_lookup(self->right, key);
+        return bstree_node_lookup(node->right, key);
 
-    return self;
+    return node;
 }
 
 static bstree_node_t *bstree_min_node(bstree_node_t *self) {
@@ -124,25 +124,25 @@ map_res_t bstree_lookup(const void *_self, const mkey_t key) {
     };
 }
 
-static bstree_node_t *bstree_node_remove(bstree_node_t *self, const char *key) {
-    if (self == NULL)
+static bstree_node_t *bstree_node_remove(bstree_node_t *node, const char *key) {
+    if (node == NULL)
         return NULL;
 
-    const int cmp = strcmp(key, self->data.key);
+    const int cmp = strcmp(key, node->data.key);
     if (cmp < 0) {
-        self->left = bstree_node_remove(self->left, key);
-        return self;
+        node->left = bstree_node_remove(node->left, key);
+        return node;
     }
     if (cmp > 0) {
-        self->right = bstree_node_remove(self->right, key);
-        return self;
+        node->right = bstree_node_remove(node->right, key);
+        return node;
     }
 
     // cmp == 0
 
-    if (self->left == NULL && self->right == NULL) {
-        free(self->data.key);
-        free(self);
+    if (node->left == NULL && node->right == NULL) {
+        free(node->data.key);
+        free(node);
         return NULL;
     }
 
@@ -153,10 +153,10 @@ static bstree_node_t *bstree_node_remove(bstree_node_t *self, const char *key) {
     //   / \
     //  2   4
 
-    if (self->left == NULL) {
-        bstree_node_t *next = self->right;
-        free(self->data.key);
-        free(self);
+    if (node->left == NULL) {
+        bstree_node_t *next = node->right;
+        free(node->data.key);
+        free(node);
         return next;
     }
 
@@ -167,10 +167,10 @@ static bstree_node_t *bstree_node_remove(bstree_node_t *self, const char *key) {
     //   / \
     //  1   3
 
-    if (self->right == NULL) {
-        bstree_node_t *next = self->left;
-        free(self->data.key);
-        free(self);
+    if (node->right == NULL) {
+        bstree_node_t *next = node->left;
+        free(node->data.key);
+        free(node);
         return next;
     }
 
@@ -184,11 +184,12 @@ static bstree_node_t *bstree_node_remove(bstree_node_t *self, const char *key) {
     //  min in the
     //  right branch
 
-    const bstree_node_t *min = bstree_min_node(self->right);
-    self->data.key = strdup(min->data.key);
-    self->data.value = min->data.value;
-    self->right = bstree_node_remove(self->right, min->data.key);
-    return self;
+    const bstree_node_t *min = bstree_min_node(node->right);
+    free(node->data.key);
+    node->data.key = strdup(min->data.key);
+    node->data.value = min->data.value;
+    node->right = bstree_node_remove(node->right, min->data.key);
+    return node;
 }
 
 int bstree_remove(void *_self, const mkey_t key) {
